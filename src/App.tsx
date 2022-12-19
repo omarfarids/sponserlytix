0e7375c952c6d42dbd04004edf67fe83455bb552
omarfarids <omar.farid007@gmail.com>
2022-12-19 21:22:55 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
 import React, { useContext } from "react";
 import CardsSection from "./components/CardsSection";
 import Chart from "./components/Chart";
-import { AppContext } from "./components/context";
 
 function App() {
-  const { data } = useContext(AppContext);
-
-  console.log(data);
-
   return (
     <div className="App">
       <CardsSection />
